7f80669bc6837e2e0d5efb449b2e7ff63af0f90e
saulhervas <saulhervasgallego@gmail.com>
2024-04-01 16:38:13 +0200

    Cambio en parrafos

--- app/src/main/java/com/saulhervas/myapplication/MainActivity.kt
+++ app/src/main/java/com/saulhervas/myapplication/MainActivity.kt
@@ -35,17 +35,16 @@ class MainActivity : AppCompatActivity() {
     private fun generarParrafoRandom(): String {
         val parrafos = listOf(
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
-                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
-                    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
             "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.",
-            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, " +
-                    "similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, ",
             "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.",
             "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, " +
                     "ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
             "Sed eget elit mauris. Integer ut tellus et dui tempus varius. Vivamus non odio orci. Integer in luctus libero. Etiam id diam eget quam pulvinar tincidunt. Vivamus accumsan nulla id tempus vestibulum",
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac magna sapien. Nulla ultricies, nulla sed finibus bibendum, magna magna malesuada nulla, a laoreet odio nulla vel nunc. Vestibulum auctor, " +
-                    "libero vel malesuada consequat, felis libero hendrerit dolor, vel dapibus tortor neque vel sem. Sed eget elit mauris. Integer ut tellus et dui tempus varius. Vivamus non odio orci. Integer in luctus libero. "
+                    "libero vel malesuada consequat, felis libero hendrerit dolor, vel dapibus tortor neque vel sem.",
+            "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
 
         )
         return parrafos.random()
